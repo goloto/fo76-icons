31a69c7b6aefece62f3dd457d83b5e8ef94694ca
goloto <mikhail@goloto.dev>
2024-06-05 16:29:14 +0300

    added BOM bytes to end of the rules file; enhance new lines in rules file

--- utils/headers-generator.ts
+++ utils/headers-generator.ts
@@ -3,25 +3,24 @@ import type { IconInfo } from "./types";
 const FILE_HEADER = `#Do Not Edit
 [version=3]
 [StartHeader]
-	Tag_0=default
-	Template_0=defaultTemplate,*
+ Tag_0=default
+ Template_0=defaultTemplate,*
 [EndHeader]`;
 const RULE_TEMPLATE = `[StartRule]
-  Header=$
-  rSig=WEAP
-  fSig=FULL
-  enabled=-1
-  tagID=default
-  noKW=0
-  anyKW=0
-  hasCompo=0
-  preProcess=0
-  fullReplace=0
-  includeOR=0
-  isFallBack=0
-  fallbackBank=0
-  regex=
-  %
+ Header=$
+ rSig=WEAP
+ fSig=FULL
+ enabled=-1
+ tagID=default
+ noKW=0
+ anyKW=0
+ hasCompo=0
+ preProcess=0
+ fullReplace=0
+ includeOR=0
+ isFallBack=0
+ fallbackBank=0
+ %
 [EndRule]`;
 
 const generate = async () => {
@@ -31,17 +30,17 @@ const generate = async () => {
 
   const result = json.reduce((accumulator: string, item) => {
     const {header, include} = item;
-    const ruleWithHeader = RULE_TEMPLATE.replace('$', replaceHeader(header));
+    const ruleWithHeader = RULE_TEMPLATE.replace('$', formatHeader(header));
     const concatenatedInclude = concatenateInclude(include);
     const ruleWithInclude = ruleWithHeader.replace('%', concatenatedInclude);
 
-    return `${accumulator}\r\r${ruleWithInclude}`;
+    return `${accumulator}\r\n\r\n${ruleWithInclude}`;
   }, `${FILE_HEADER}`) as string;
 
-  await Bun.write('./headers/rules.txt', result);
+  await Bun.write('./headers/rules.txt', createUtf8BomString(result));
 }
 
-const replaceHeader = (str?: string): string => {
+const formatHeader = (str?: string): string => {
   if (!str) {
     return '';
   }
@@ -55,10 +54,17 @@ const concatenateInclude = (array?: string[]): string => {
   }
 
   return array.reduce((accumulator, includeItem, index) => {
-    const newLine = index === 0 ? '' : '\r  '
+    const newLine = index === 0 ? '' : '\r\n '
 
     return `${accumulator}${newLine}Include_${index}=${includeItem}`;
   }, '');
+}
+
+const createUtf8BomString = (str: string): Blob => {
+  return new Blob([
+    new Uint8Array([0xEF, 0xBB, 0xBF]),
+    str
+  ], { type: "text/plain;charset=utf-8" });
 }
 
 await generate();
