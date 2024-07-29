1870b3e347f9ac914605394ad7e23f45299647b0
Mikhail Goloto <mikhail@goloto.dev>
2024-07-29 23:03:27 +0300

    added notes rules as esm

---
+++ rules/notes.ts
@@ -0,0 +1,45 @@
+import { ICON_NOTES } from '../generated/icons-enum';
+import { LeftSignature, RightSignature } from '../utils/const';
+import type { Rule } from '../utils/types';
+
+export const NOTES_RULES: Rule[] = [
+  {
+    order: 0,
+    iconName: ICON_NOTES.Plan,
+    leftSignature: LeftSignature.Book,
+    rightSignature: RightSignature.FULL,
+    isInjected: false, isDeleted: false,
+    isAnyKeyword: false,
+    isInclusiveOr: false,
+    isFullReplaced: false,
+    header: '0x22ae',
+    include: ['ekwd|ObjectTypeRecipe'],
+    exclude: [],
+  },
+  {
+    order: 1,
+    iconName: ICON_NOTES.Note,
+    leftSignature: LeftSignature.Book,
+    rightSignature: RightSignature.FULL,
+    isInjected: false, isDeleted: false,
+    isAnyKeyword: false,
+    isInclusiveOr: false,
+    isFullReplaced: false,
+    header: '0x22af',
+    include: [],
+    exclude: ['ekwd|ObjectTypeRecipe', 'ekwd|MagazineKeyword'],
+  },
+  {
+    order: 2,
+    iconName: ICON_NOTES.Check,
+    leftSignature: LeftSignature.Book,
+    rightSignature: RightSignature.FULL,
+    isInjected: false, isDeleted: false,
+    isAnyKeyword: false,
+    isInclusiveOr: false,
+    isFullReplaced: false,
+    header: '0x22b0',
+    include: [],
+    exclude: [],
+  },
+];
