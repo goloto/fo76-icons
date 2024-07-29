9205c5add97c1f94abfe408281f3c46eea46a2dc
Mikhail Goloto <mikhail@goloto.dev>
2024-07-29 23:36:08 +0300

    fixed type errors in readIcons

--- utils/builders/read-icons.ts
+++ utils/builders/read-icons.ts
@@ -1,10 +1,10 @@
 import { ICONS_DIRECTORY } from '../const';
-import type { CategoryOrder } from '../types';
+import type { CategoryOrder, IconNames } from '../types';
 import { readdir } from 'node:fs/promises';
 
 export const readIcons = async (
   categoryOrder: CategoryOrder[]
-): Promise<Record<string, string[]>> => {
+): Promise<Record<string, IconNames[]>> => {
   const paths = await categoryOrder.reduce(async (accumulator, category) => {
     const awaitedAccumulator = await accumulator;
     const categoryIconPaths = await readdir(
