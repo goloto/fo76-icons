b8d69414444019143291a2b076e673de09b4f818
Mikhail Goloto <mikhail@goloto.dev>
2024-08-04 20:18:49 +0300

    added order correction to sort-rules script

--- utils/sort-rules.ts
+++ utils/sort-rules.ts
@@ -1,19 +1,46 @@
-import ts from 'typescript';
+import ts, { type PropertyAssignment } from 'typescript';
+
+const visitPropertyAssignments =
+  (node: ts.Node, index: number) => (node: ts.Node) => {
+    if (
+      node.kind === ts.SyntaxKind.PropertyAssignment &&
+      node.getChildAt(0, inputFile).getText(inputFile) === 'order'
+    ) {
+      return ts.factory.updatePropertyAssignment(
+        node as PropertyAssignment,
+        ts.factory.createIdentifier('order'),
+        ts.factory.createNumericLiteral(index)
+      );
+    }
+
+    return node;
+  };
 
 const visit = (node: ts.Node): ts.Node => {
   if (node.kind === ts.SyntaxKind.ArrayLiteralExpression) {
     const elements = (node as ts.ArrayLiteralExpression)
       .elements as ts.NodeArray<ts.ObjectLiteralExpression>;
-    const sortedElements = elements.toSorted((objA, objB) => {
-      const orderPropA = objA.properties.find(
-        (property) => property?.name?.getText(inputFile) === 'order'
-      ) as ts.PropertyAssignment;
-      const orderPropB = objB.properties.find(
-        (property) => property?.name?.getText(inputFile) === 'order'
-      ) as ts.PropertyAssignment;
+    const sortedElements = elements
+      .toSorted((objA, objB) => {
+        const orderPropA = objA.properties.find(
+          (property) => property?.name?.getText(inputFile) === 'order'
+        ) as ts.PropertyAssignment;
+        const orderPropB = objB.properties.find(
+          (property) => property?.name?.getText(inputFile) === 'order'
+        ) as ts.PropertyAssignment;
+        const orderValueA = Number(orderPropA.initializer.getText(inputFile));
+        const orderValueB = Number(orderPropB.initializer.getText(inputFile));
 
-      return Number(orderPropA) - Number(orderPropB);
-    });
+        return orderValueA - orderValueB;
+      })
+      .map((node, index) =>
+        ts.visitEachChild(
+          node,
+          visitPropertyAssignments(node, index),
+          undefined
+        )
+      );
+
     const newNode = ts.factory.createArrayLiteralExpression(
       sortedElements,
       true
@@ -32,7 +59,7 @@ const transformerFactory: ts.TransformerFactory<ts.Node> =
 
 const inputFileString = await Bun.file('./rules/__test.ts').text();
 const inputFile = ts.createSourceFile(
-  'x.ts',
+  './rules/__test.ts',
   inputFileString,
   ts.ScriptTarget.Latest,
   undefined
