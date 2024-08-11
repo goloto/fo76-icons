51aa2520a1e7c53ea8f8b7668af2e9fa725a06d3
Mikhail Goloto <mikhail@goloto.dev>
2024-08-11 16:20:12 +0300

    building FontConfig now uses Icons instead of iconNames

--- src/scripts/build-all/index.ts
+++ src/scripts/build-all/index.ts
@@ -1,10 +1,10 @@
 import { buildIconRules } from './icons-rules';
 import { buildHeaders } from './icons-headers';
-import { buildFontConfig } from './font-config';
 import { readIcons } from './read-icons';
 import { generateCategoriesFile } from '@/scripts/generate-categories-file';
 import { generateIconsEnumFile } from '@/scripts/generate-icons-enum-file';
 import { generateIcons } from '@/utils/generate-icons';
+import { generateFontConfigFile } from '@/utils/generate-font-config-file';
 import { generateUnicodeMapFile } from '@/utils/generate-unicode-map-file';
 
 const build = async () => {
@@ -16,7 +16,7 @@ const build = async () => {
   const iconRules = await buildIconRules(iconNames);
 
   await buildHeaders(iconRules);
-  await buildFontConfig(iconRules);
+  await generateFontConfigFile(icons);
   await generateUnicodeMapFile(icons);
 };
 
