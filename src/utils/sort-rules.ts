98cac44f8252a349213c90d9084c1a33b800f0af
Mikhail Goloto <mikhail@goloto.dev>
2024-08-24 22:28:27 +0300

    sort-rules now sort all files in /src/rules

--- src/utils/sort-rules.ts
+++ src/utils/sort-rules.ts
@@ -1,8 +1,20 @@
 import { sortArrayExpression } from '@/ast-utils/sort-rules';
+import { RULES_DIRECTORY } from '@/constants';
+import { readdir } from 'node:fs/promises';
 
 export const sortRules = async () => {
-  const file = await Bun.file('./src/rules/aid.ts').text();
-  const sortedFile = sortArrayExpression(file);
+  const ruleFiles = await readdir(RULES_DIRECTORY);
 
-  await Bun.write('./src/rules/aid_sorted.ts', sortedFile);
+  for (const filePath of ruleFiles) {
+    console.log(filePath);
+    const file = await Bun.file(`${RULES_DIRECTORY}/${filePath}`).text();
+
+    try {
+      const sortedFile = sortArrayExpression(file);
+
+      await Bun.write(`${RULES_DIRECTORY}/${filePath}`, sortedFile);
+    } catch {
+      throw new Error(`Error while sorting ${filePath}`);
+    }
+  }
 };
