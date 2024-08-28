bd21bed441ecfff134a9531bc6f46572e33e7839
Mikhail Goloto <mikhail@goloto.dev>
2024-08-28 10:04:55 +0300

    added snapshot tests for rest rules

--- src/tests/rules.test.ts
+++ src/tests/rules.test.ts
@@ -1,31 +1,59 @@
 import { describe, test, expect } from 'bun:test';
 
 describe('Testing all rules with snapshots', () => {
-  test('Testing WEAP rules', async () => {
-    const weaponsXml = Bun.file('./src/tests/rules-xml/WEAP.xml');
-    const weaponsString = await weaponsXml.text();
-
-    expect(weaponsString).toMatchSnapshot();
-  });
-
-  test('Testing ALCH rules', async () => {
-    const alchemyXml = Bun.file('./src/tests/rules-xml/ALCH.xml');
+  test('Testing ALCH:FULL rules', async () => {
+    const alchemyXml = Bun.file('./src/tests/rules-xml/ALCH:FULL.xml');
     const alchemyString = await alchemyXml.text();
 
     expect(alchemyString).toMatchSnapshot();
   });
 
-  test('Testing MISC rules', async () => {
-    const miscXml = Bun.file('./src/tests/rules-xml/MISC.xml');
+  test('Testing AMMO:FULL rules', async () => {
+    const ammunitionXml = Bun.file('./src/tests/rules-xml/AMMO:FULL.xml');
+    const ammunitionString = await ammunitionXml.text();
+
+    expect(ammunitionString).toMatchSnapshot();
+  });
+
+  test('Testing ARMO:FULL rules', async () => {
+    const armorXml = Bun.file('./src/tests/rules-xml/ARMO:FULL.xml');
+    const armorString = await armorXml.text();
+
+    expect(armorString).toMatchSnapshot();
+  });
+
+  test('Testing BOOK:FULL rules', async () => {
+    const bookXml = Bun.file('./src/tests/rules-xml/BOOK:FULL.xml');
+    const bookString = await bookXml.text();
+
+    expect(bookString).toMatchSnapshot();
+  });
+
+  test('Testing INNR:WNAM rules', async () => {
+    const innrXml = Bun.file('./src/tests/rules-xml/INNR:WNAM.xml');
+    const innrString = await innrXml.text();
+
+    expect(innrString).toMatchSnapshot();
+  });
+
+  test('Testing KEYM:FULL rules', async () => {
+    const keysXml = Bun.file('./src/tests/rules-xml/KEYM:FULL.xml');
+    const keysString = await keysXml.text();
+
+    expect(keysString).toMatchSnapshot();
+  });
+
+  test('Testing MISC:FULL rules', async () => {
+    const miscXml = Bun.file('./src/tests/rules-xml/MISC:FULL.xml');
     const miscString = await miscXml.text();
 
     expect(miscString).toMatchSnapshot();
   });
 
-  test('Testing ARMO rules', async () => {
-    const armorXml = Bun.file('./src/tests/rules-xml/ARMO.xml');
-    const armorString = await armorXml.text();
+  test('Testing WEAP:FULL rules', async () => {
+    const weaponsXml = Bun.file('./src/tests/rules-xml/WEAP:FULL.xml');
+    const weaponsString = await weaponsXml.text();
 
-    expect(armorString).toMatchSnapshot();
+    expect(weaponsString).toMatchSnapshot();
   });
 });
